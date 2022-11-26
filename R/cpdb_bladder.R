9bec566d99556b33e7e4fb9597276a7bca956e71
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2022-11-26 15:46:32 -0500

    Update cpdb_bladder.R

--- R/cpdb_bladder.R
+++ R/cpdb_bladder.R
@@ -1,3 +1,32 @@
+library(Seurat)
+library(SeuratObject)
+library(Matrix)
+
+library(openxlsx)
+library(readxl)
+library(readr)
+library(scales)
+library(stringr)
+library(data.table)
+
+library(gtools)
+library(ggplot2)
+library(RColorBrewer)
+library(patchwork)
+library(reshape2)
+library(gridExtra)
+library(cowplot)
+library(ggrepel)
+library(lattice)
+library(viridis)
+
+library(Hmisc)
+library(dplyr)
+library(tidyr)
+library(purrr)
+
+
+
 # The testing is done per group (total 3) based on BCG treatment status. 
 
 
@@ -53,31 +82,95 @@ return(sub_obj)
 }
 
 
-# read obj and subset by group
+### 1. Read obj and subset by group ###
+out<-'/home/sonas/cellphonedb/data/scRNA/'
 
-obj<-readRDS('dta_cancer_final.rds')
+obj<-readRDS('/home/sonas/tingalab/Surbhi/PROJECTS_tinglab_drive/BLADDER/scRNA/DATA/dta_cancer_final.rds')
+
+### 2. Subet by treatment group ###
 Idents(obj)<-'Groups'
 sub_nw<-subset(obj,ident='Naive_w') # 32909 cells
 sub_nwo<-subset(obj,ident='Naive_wo') # 103534 cells
 sub_rec<-subset(obj,ident='Recurrent') # 113786 cells
 
-# now subset by compartment
-Idents(sub_nw)<-'low_res_compartment'
-sub_nw_ui<-subset(sub_nw,ident=c('immune','uroepithelial')) # 3379 and 29491 cells respectively
-                  
-# check cell count
-cell_tab<-table(sub_nw_ui@meta.data$cell.names) %>% as.data.frame()
+### 3. Now subset by compartment ###
+
+## Naive_w ##
+#Idents(sub_nw)<-'low_res_compartment'
+#sub_nw_ui<-subset(sub_nw,ident=c('immune','uroepithelial')) # 3379 and 29491 cells respectively
+
+## Naive_wo ##
+#Idents(sub_nwo)<-'low_res_compartment'
+#sub_nwo_ui<-subset(sub_nwo,ident=c('immune','uroepithelial')) #  20164 and  82945 cells respectively
+
+## Recurrent ##
+#Idents(sub_rec)<-'low_res_compartment'
+#sub_rec_ui<-subset(sub_rec,ident=c('immune','uroepithelial')) #  31468 and  80402 cells respectively
+
+
+### 4. check cell count ###
+
+## Naive_w ##
+cell_tab<-table(sub_nw@meta.data$cell.names) %>% 
+  as.data.frame()
+
 colnames(cell_tab)<-c('cell_type','count')
-write.table(cell_tab,'uro_imm_cell_count_Naive_w.txt',sep='\t',row.names=FALSE)
+write.table(cell_tab,paste0(out,'uro_imm_cell_count_Naive_w.txt'), sep='\t',row.names=FALSE)
 
-# Now create cpdb inputs
 
+## Naive_wo ##
+cell_tab<-table(sub_nwo@meta.data$cell.names) %>% 
+  as.data.frame()
+
+colnames(cell_tab)<-c('cell_type','count')
+write.table(cell_tab,paste0(out,'uro_imm_cell_count_Naive_wo.txt'), sep='\t',row.names=FALSE)
+
+
+## Recurrent ##
+cell_tab<-table(sub_rec@meta.data$cell.names) %>% 
+  as.data.frame()
+
+colnames(cell_tab)<-c('cell_type','count')
+write.table(cell_tab,paste0(out,'uro_imm_cell_count_Recurrent.txt'), sep='\t',row.names=FALSE)
+
+
+### 5. Now create cpdb inputs ###
+
+## a) Naive_w ##
 prefix<-'Naive_w'
 
 sub<-metadata_cutoff_based_subset(obj=sub_nw,meta='cell.names',cutoff=10) # 30696 cells remain
+
+# Save metadata #
+save_metadata(sub,meta='cell.names',fname=paste0(out,prefix,'_metadata.txt'))
+
+# Save norm expression matrix #
+
+writeMM(sub@assays$RNACleaned@data, file = paste0(out,prefix,'_matrix.mtx'))
+
+#save_norm_exp_mat(sub,assay='RNACleaned',fname=paste0(out,prefix,'_counts.txt'))
+
+
+## b) Naive_wo ##
+
+prefix<-'Naive_wo'
+sub<-metadata_cutoff_based_subset(obj=sub_nwo,meta='cell.names',cutoff=10) # 
 ### Save metadata ###
 save_metadata(sub,meta='cell.names',fname=paste0(out,prefix,'_metadata.txt'))
 ### Save norm expression matrix ###
-save_norm_exp_mat(sub,assay='RNACleaned',fname=paste0(out,prefix,'_counts.txt'))
+#save_norm_exp_mat(sub,assay='RNACleaned',fname=paste0(out,prefix,'_counts.txt'))
+writeMM(sub@assays$RNACleaned@data, file = paste0(out,prefix,'_matrix.mtx'))
 
-                  
+
+## c) Recurrent ##
+prefix<-'Recurrent'
+
+sub<-metadata_cutoff_based_subset(obj=sub_rec,meta='cell.names',cutoff=10) #  cells remain
+
+### Save metadata ###
+save_metadata(sub,meta='cell.names',fname=paste0(out,prefix,'_metadata.txt'))
+
+### Save norm expression matrix ###
+#save_norm_exp_mat(sub,assay='RNACleaned',fname=paste0(out,prefix,'_counts.txt'))
+writeMM(sub@assays$RNACleaned@data, file = paste0(out,prefix,'_matrix.mtx'))
+
