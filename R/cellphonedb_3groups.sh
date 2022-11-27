be1b55ed42a672d54511b4895aa9331beb484ef2
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2022-11-27 00:31:44 -0500

    Update and rename cellphonedb.sh to cellphonedb_3groups.sh

--- R/cellphonedb_3groups.sh
+++ R/cellphonedb_3groups.sh
@@ -1,9 +1,12 @@
 # Run cellphonedb
 
-out_dir='/Users/sonas/Documents/PROJECTS/BLADDER/RESULTS/cellphonedb/'
+input_dir='/Users/sonas/Documents/PROJECTS/BLADDER/RESULTS/cellphonedb/inputs/'
+out_dir='/Users/sonas/Documents/PROJECTS/BLADDER/RESULTS/cellphonedb/group_comparison/'
+
 prefix='Naive_w'
-counts=${out_dir}${prefix}_counts.txt
-meta=${out_dir}${prefix}_metadata.txt
+counts=${input_dir}${prefix}_matrix.mtx
+meta=${input_dir}${prefix}_metadata.txt
+
 cellphonedb method statistical_analysis ${meta} ${counts} --counts-data gene_name --project-name ${prefix} --output-path ${out_dir} --verbose --pvalue 0.001
 
 
