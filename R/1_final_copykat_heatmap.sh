ec3043a7beb3b7d53d6fd51469202c61512a18f8
Surbhi Sona <57156171+2019surbhi@users.noreply.github.com>
2023-01-06 13:02:36 -0500

    Update 1_final_copykat_heatmap.sh

--- R/1_final_copykat_heatmap.sh
+++ R/1_final_copykat_heatmap.sh
@@ -19,5 +19,14 @@ Rscript final_copyKat_heatmap.R 6,7,35,10,36,43,44,45 recurrent_pairs 10 saved
 
 #Rscript save_mat.R 6,7,35,10,36,43,44,45 recurrent_pairs 8 notsaved
 
-Rscript final_copyKat_heatmap.R 1,6,7,8,9,10,11,13,17,19,21,23,24,25,29,31,33,34,35,36,39,40,42,43,44,45,46 samples27 10 saved
+#Rscript final_copyKat_heatmap.R 1,6,7,8,9,10,11,13,17,19,21,23,24,25,29,31,33,34,35,36,39,40,42,43,44,45,46 samples27 10 saved
 
+
+sub=0.85 #(90% fails because of R vector limitation)
+
+mode='subsampled'
+
+out=/home/sonas/beegfs/results/scRNA/bladder_copyKat_heatmap/final_heatmap/${sub}/
+mkdir ${out}
+
+Rscript final_copyKat_heatmap.R 1,6,7,8,9,10,11,13,17,19,21,23,24,25,29,31,33,34,35,36,39,40,42,43,44,45,46 samples27 10 ${mode} ${sub}
